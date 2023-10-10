26a8208d798c392367a25ba7c24ae48214b9645a
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-10-10 14:45:16 +0000

    Add directors and before_action with set_movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -14,7 +16,6 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
@@ -32,12 +33,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-    
     if @movie.update(self.movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
     else
@@ -46,8 +44,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
-
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
@@ -57,5 +53,9 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
